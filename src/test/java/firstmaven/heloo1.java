207a08e0ef8d0539acc73d0d4242846e754b7959
mrishnamoorthy <mrishnamoorthy@lm-maa-26500992.paypalcorp.com>
2017-10-20 00:19:51 -0700

    done

---
+++ src/test/java/firstmaven/heloo1.java
@@ -0,0 +1,14 @@
+package firstmaven;
+
+import org.testng.annotations.Test;
+
+@Test(priority =1)
+public class heloo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println("Hello1");
+ 
+	}
+
+}
---
+++ src/test/java/firstmaven/proj22.java
@@ -0,0 +1,21 @@
+package firstmaven;
+
+import org.testng.annotations.Test;
+@Test(priority =2)
+public class proj22 {
+
+	
+
+	
+	
+
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+		 System.out.println("Hello1");
+			}
+
+		
+
+	}
+
+
